60d4772ae5d95ba6b07d50a964875e57b366287c
Bruno Novais <bruno.br001@gmail.com>
2020-09-10 01:20:56 +0100

    Landing structure Style v_1

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import './style.css'
+import "./style.css";
 
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
@@ -11,28 +11,30 @@ import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 function Landing() {
   return (
     <div id="page-landing" className="container">
-      <div className="logo-container">
-        <img src={logoImg} alt="logo" />
-        <h2>Your online study platform</h2>
-      </div>
+      <div className="page-landing-content">
+        <div className="logo-container">
+          <img src={logoImg} alt="logo" />
+          <h2>Your online study platform</h2>
+        </div>
 
-      <img src={landingImg} alt="landing image" className="hero-image" />
-      <div className="buttons-container">
-        <a href="" className="study">
-          <img src={studyIcon} alt="study" />
-          Study
-        </a>
+        <img src={landingImg} alt="landing main" className="hero-image" />
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="study" />
+            Study
+          </a>
 
-        <a href="" className="give-classes">
-          <img src={giveClassesIcon} alt="give classes" />
-          Give Classes
-        </a>
-      </div>
+          <a href="" className="give-classes">
+            <img src={giveClassesIcon} alt="give classes" />
+            Give Classes
+          </a>
+        </div>
 
-      <span className="total-connections">
+        <span className="total-connections">
           Total connections - 200
-          <img src={purpleHeartIcon} alt="purple heart"/>
-      </span>
+          <img src={purpleHeartIcon} alt="purple heart" />
+        </span>
+      </div>
     </div>
   );
 }
