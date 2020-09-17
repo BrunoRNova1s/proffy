44de699ead7cbc24631b354884e106a0138c43ee
Bruno Novais <bruno.br001@gmail.com>
2020-09-18 00:18:18 +0100

    teacher-item style v_1

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 import "./style.css";
 
 function TeacherList() {
@@ -25,6 +27,41 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
+              alt="Me"
+            />
+            <div>
+              <strong>Bruno Novais</strong>
+              <span>Math</span>
+            </div>
+          </header>
+
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+            <br/>
+             When an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. 
+          </p>
+
+          <footer>
+            <p>
+              Price/hour
+              <strong>100 €</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="contact"/>
+              Contact
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
