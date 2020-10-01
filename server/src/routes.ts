5c1f63bfffc565af7ba43e3161c302fa9350d75a
Bruno Novais <bruno.br001@gmail.com>
2020-10-02 00:28:02 +0100

    route classe_schedule - data into table classe_schedule

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,17 +1,45 @@
 import express from "express";
 import db from "./database/connection";
+import convertHourToMinutes from "./utils/convertHourToMinutes";
 
 const routes = express.Router();
+
+interface scheduleItem {
+  week_day: number;
+  from: string;
+  to: string
+}
 
 routes.post("/classes", async (request, response) => {
   const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
 
-  await db("users").insert({
+  const insertedUserIds = await db("users").insert({
     name,
     avatar,
     whatsapp,
     bio,
   });
+
+  const user_id = insertedUserIds[0]
+
+  const insertedClassesIds = await db('classes').insert({
+    subject,
+    cost,
+    user_id,
+  })
+
+  const class_id = insertedClassesIds[0]
+
+  const classSchedule = schedule.map((scheduleItem: scheduleItem) => {
+    return {
+      class_id,
+      week_day: scheduleItem.week_day,
+      from: convertHourToMinutes(scheduleItem.from),
+      to: convertHourToMinutes(scheduleItem.to)
+    }
+  })
+
+  await db('class_schedule').insert(classSchedule)
 
   return response.send();
 });
