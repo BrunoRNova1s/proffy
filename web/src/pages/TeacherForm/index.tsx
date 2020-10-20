199920456202b4960aedc5afd8a21d980e5b6573
Bruno Novais <bruno.br001@gmail.com>
2020-10-21 00:52:57 +0100

    add description - teacher form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import PageHeader from '../../components/PageHeader'
+import React from "react";
+import PageHeader from "../../components/PageHeader";
 
 function TeacherForm() {
-    return (
-        <div id="page-teacher-list" className="container">
-        <PageHeader title="You want to teach, amazing!" />
-
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader
+        title="You want to teach, amazing!"
+        description="the first step is to fill out the form"
+      />
     </div>
-    )
+  );
 }
 
-export default TeacherForm
+export default TeacherForm;
