a52cacc09f362e164990495ae1c0dce32e2eb793
Bruno Novais <bruno.br001@gmail.com>
2020-10-27 01:05:22 +0000

    update teacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
+import "./styles.css";
 
 function TeacherForm() {
   return (
@@ -8,6 +9,26 @@ function TeacherForm() {
         title="You want to teach, amazing!"
         description="the first step is to fill out the form"
       />
+
+      <main>
+        <fieldset>
+          <legend>Your data</legend>
+
+          <div className="input-block">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="Whatsapp">Whatsapp</label>
+            <input type="text" id="Whatsapp" />
+          </div>
+        </fieldset>
+      </main>
     </div>
   );
 }
